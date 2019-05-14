f02dbfdd45797e27aa0ee90f4de92bc2b47723b8
Jordan Woodruff <jordan@neko-chan.local>
2019-05-14 00:03:04 -0500

    comments

--- AlgorithmsAndDataStructures/Algorithms/Sorting/InsertionSorting.cs
+++ AlgorithmsAndDataStructures/Algorithms/Sorting/InsertionSorting.cs
@@ -6,19 +6,27 @@ namespace Algorithms.Sorting
         public static IList<T> InsertionSort<T>(this IList<T> pList)
         {
             int i, j;
+
+            // use the default comparer
             var comparer = Comparer<T>.Default;
 
+            // start at the second element
             for(i = 1; i < pList.Count; i++)
             {
+                // get the secord element
                 T value = pList[i];
+
+                // choose the first index to compare it to
                 j = i - 1;
 
+                // if the value is greater than swap and repeat
                 while(j >= 0 && comparer.Compare(pList[j], value) > 0)
                 {
                     pList[j + 1] = pList[j];
                     j--;
                 }
 
+                // finally, just swap the last value
                 pList[j + 1] = value;
             }
 
