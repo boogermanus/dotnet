a94f5b8504a684e50e2a52bcb138e6600f3c7d65
Jordan Woodruff <jordan@neko-chan.local>
2019-05-14 17:49:26 -0500

    Add db scaffold

---
+++ SimpleStupid/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SimpleStupid.Data
+{
+    public partial class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+        }
+    }
+}
