4499a69a03be99550b1b3c35bab9fb5d8a3fda62
Jordan Woodruff <jordan@neko-chan.local>
2019-05-16 22:16:59 -0500

    don't use undfined contstants

--- AlgorithmsAndDataStructures/Algorithms/Sorting/MergeSorting.cs
+++ AlgorithmsAndDataStructures/Algorithms/Sorting/MergeSorting.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 public static class MergeSorting
 {
+    private const int ONE = 1;
+    private const int MIDDLE = 2;
     public static IList<T> MergeSort<T>(this IList<T> pList, int pFirst, int pLast) where T : IComparable
     {
-        if(pLast - pFirst < 1)
+        if(pLast - pFirst < ONE)
             return new List<T>() {pList[pLast]};
         
-        var midpoint = (pFirst + pLast)/2;
+        var midpoint = (pFirst + pLast)/MIDDLE;
 
         var left = pList.MergeSort(pFirst, midpoint);
         var right = pList.MergeSort(midpoint+1, pLast);
@@ -17,16 +19,18 @@ public static class MergeSorting
 
     public static IList<T> MergeSort<T>(this IList<T> pList) where T : IComparable
     {
-        return pList.MergeSort(0, pList.Count - 1);
+        return pList.MergeSort(0, pList.Count - ONE);
     }
 
     private static IList<T> Merge<T>(IList<T> pLeft, IList<T> pRight) where T : IComparable
     {
         var newList = new List<T>();
 
-        // 
+        // because we are removing elements from the list
+        // we are still saving memeory
         while(pLeft.Count != 0 && pRight.Count != 0)
         {
+            // take the first element from each list
             var leftMin = pLeft[0];
             var rightMin = pRight[0];
 
@@ -44,6 +48,7 @@ public static class MergeSorting
             }
         }
         
+        // add what is left
         newList.AddRange(pLeft);
         newList.AddRange(pRight);
 
