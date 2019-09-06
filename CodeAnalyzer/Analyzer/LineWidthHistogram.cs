cbde049a9458fa76dab4bde725fd7091c7a5f362
Jordan Woodruff <jordan@neko-chan.local>
2019-09-06 18:40:20 -0500

    new methods/properties/tests

--- CodeAnalyzer/Analyzer/LineWidthHistogram.cs
+++ CodeAnalyzer/Analyzer/LineWidthHistogram.cs
@@ -1,12 +1,18 @@
+using System.Linq;
 using System.Collections.Generic;
 
 namespace Analyzer
 {
     public class LineWidthHistogram
     {
+        #region Members
         public Dictionary<int, int> LineWidths { get; set; } = new Dictionary<int, int>();
         public int LineCount { get; set; }
+        public int WidestLine => OrderedWidths.OrderByDescending(i => i).FirstOrDefault();
 
+        public List<int> OrderedWidths => LineWidths.Keys.OrderBy(i => i).ToList();
+        #endregion
+        #region Methods
         public void AddLine(string pLine)
         {
             if(LineWidths.ContainsKey(pLine.Length))
@@ -16,5 +22,6 @@ namespace Analyzer
                 
             LineCount++;
         }
+        #endregion
     }
 }
--- CodeAnalyzer/UnitTests/LineWidthHistogramTests.cs
+++ CodeAnalyzer/UnitTests/LineWidthHistogramTests.cs
@@ -6,38 +6,61 @@ namespace UnitTests
     [TestFixture]
     public class LineWidthHistogramTests
     {
+        [SetUp]
+        public void SetUp()
+        {
+            Histogram = new LineWidthHistogram();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Histogram = new LineWidthHistogram();
+        }
         public const string TEST_STRING_1 = "testing";
         public const string TEST_STRING_2 = "testing again";
+
+        private LineWidthHistogram Histogram;
+
         [Test]
         public void LineWidthHistogram_Should_Init()
         {
-            var histo = new LineWidthHistogram();
+            Assert.That(Histogram.LineCount, Is.Zero);
+            Assert.That(Histogram.LineWidths, Is.Empty);
 
-            Assert.That(histo.LineCount, Is.Zero);
-            Assert.That(histo.LineWidths, Is.Empty);
+            Assert.That(Histogram.WidestLine, Is.Zero);
         }
 
         [Test]
         public void AddLine_Should_AddTo_Dict_And_Increase_LineCount()
         {
-            var histo = new LineWidthHistogram();
+            Histogram.AddLine(TEST_STRING_1);
 
-            histo.AddLine(TEST_STRING_1);
-
-            Assert.That(histo.LineCount, Is.EqualTo(1));
-            Assert.That(histo.LineWidths.Count, Is.EqualTo(1));
+            Assert.That(Histogram.LineCount, Is.EqualTo(1));
+            Assert.That(Histogram.LineWidths.Count, Is.EqualTo(1));
         }
 
         [Test]
         public void AddLine_Should_CreateHistogram()
         {
-            var histo = new LineWidthHistogram();
+            Histogram.AddLine(TEST_STRING_1);
+            Histogram.AddLine(TEST_STRING_2);
 
-            histo.AddLine(TEST_STRING_1);
-            histo.AddLine(TEST_STRING_2);
+            Assert.That(Histogram.LineWidths[TEST_STRING_1.Length], Is.EqualTo(1));
+            Assert.That(Histogram.LineWidths[TEST_STRING_2.Length], Is.EqualTo(1));
 
-            Assert.That(histo.LineWidths[TEST_STRING_1.Length], Is.EqualTo(1));
-            Assert.That(histo.LineWidths[TEST_STRING_2.Length], Is.EqualTo(1));
+            Histogram.AddLine(TEST_STRING_2);
+
+            Assert.That(Histogram.LineWidths[TEST_STRING_2.Length], Is.EqualTo(2));
+        }
+
+        [Test]
+        public void WidestLine_Should_Return_The_WidestLine()
+        {
+            Histogram.AddLine(TEST_STRING_1);
+            Histogram.AddLine(TEST_STRING_2);
+
+            Assert.That(Histogram.WidestLine, Is.EqualTo(TEST_STRING_2.Length));
         }
     }
 }
