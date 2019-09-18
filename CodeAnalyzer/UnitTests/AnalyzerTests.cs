05209f928d2f53b4e88fbd4495ce471438c5ab32
Jordan Woodruff <jordan@neko-chan.local>
2019-09-18 18:43:53 -0500

    formatting

--- CodeAnalyzer/UnitTests/AnalyzerTests.cs
+++ CodeAnalyzer/UnitTests/AnalyzerTests.cs
@@ -9,15 +9,18 @@ namespace UnitTests
         [Test]
         public void Analyzer_Should_Throw_ArugmentException_ValidPath()
         {
-            Assert.That(() => new CodeAnalyzer(string.Empty), Throws.ArgumentException.With.Message.Contains("valid"));
-            Assert.That(() => new CodeAnalyzer("   "), Throws.ArgumentException.With.Message.Contains("valid"));
+            Assert.That(() => new CodeAnalyzer(string.Empty),
+                Throws.ArgumentException.With.Message.Contains("valid"));
+            Assert.That(() => new CodeAnalyzer("   "),
+                Throws.ArgumentException.With.Message.Contains("valid"));
         }
 
         [Test]
         public void Analyzer_Should_Throw_ArgumentException_NotFound()
         {
             // you should not have a directory with this name...anywhere
-            Assert.That(() => new CodeAnalyzer("bladh blahdhdhad"), Throws.ArgumentException.With.Message.Contains("not found"));
+            Assert.That(() => new CodeAnalyzer("bladh blahdhdhad"),
+                Throws.ArgumentException.With.Message.Contains("not found"));
         }
     }
 }
