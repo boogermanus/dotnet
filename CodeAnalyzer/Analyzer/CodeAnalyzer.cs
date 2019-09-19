bf629a50c134f6f8f56725d951f15ed23a0a198d
Jordan Woodruff <jordan@neko-chan.local>
2019-09-18 19:34:09 -0500

    Start Analyze method

--- CodeAnalyzer/Analyzer/CodeAnalyzer.cs
+++ CodeAnalyzer/Analyzer/CodeAnalyzer.cs
@@ -1,12 +1,15 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
 namespace Analyzer
 {
     public class CodeAnalyzer
     {
         private string _rootDirectoryPath;
         private string _fileFilter = "*.cs"; // default to C# files
+        private LineWidthHistogram _histogram = new LineWidthHistogram();
 
         public CodeAnalyzer(string pRootDirectoryPath, string pFileFilter = null)
         {
@@ -24,6 +27,33 @@ namespace Analyzer
             // return the full path name of the specified directory even if
             // the path was relative.
             return new DirectoryInfo(pRootDirectoryPath).FullName;
+        }
+
+        public void Analyze()
+        {
+            var filesToAnalyze = GetFilesToAnalyze();
+
+            var histograms = filesToAnalyze
+                .Select(file => GetHistogramForFile(file));
+        }
+
+        private List<string> GetFilesToAnalyze()
+        {
+            return Directory
+                .GetFiles(_rootDirectoryPath, _fileFilter, SearchOption.AllDirectories)
+                .ToList();
+        }
+
+        private static LineWidthHistogram GetHistogramForFile(string pFullFilePath)
+        {
+            var histogram = new LineWidthHistogram();
+            
+            File
+                .ReadAllLines(pFullFilePath)
+                .ToList()
+                .ForEach(line => histogram.AddLine(line));
+
+            return histogram;
         }
     }
 }
