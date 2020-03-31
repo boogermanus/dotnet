0bf2af1f7f7300d4dea77bd5cc4d1b283df7b553
jordan.woodruff <Jordan.Woodruff@tylertech.com>
2020-03-31 14:01:30 -0500

    Carrots

---
+++ DesignPatterns/ObserverPattern/ConcreteSubject/Carrots.cs
@@ -0,0 +1,9 @@
+using ObserverPattern.Subject;
+
+namespace ObserverPattern.ConcreteSubject
+{
+    public class Carrots : Veggies
+    {
+        public Carrots(double price) : base(price) { }
+    }
+}
