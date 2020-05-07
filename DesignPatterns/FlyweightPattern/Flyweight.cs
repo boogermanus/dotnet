0d44fe3cd854511d357086e3d2808ab70feccd79
jordan.woodruff <Jordan.Woodruff@tylertech.com>
2020-05-07 15:31:23 -0500

    intital commit

---
+++ DesignPatterns/FlyweightPattern/Flyweight.cs
@@ -0,0 +1,12 @@
+namespace FlyweightPattern.Flyweight
+{
+    public abstract class Slider
+    {
+        protected string Name;
+        protected string Cheese;
+        protected string Toppings;
+        protected decimal Price;
+
+        public abstract void Display(int orderTotal);
+    }
+}
