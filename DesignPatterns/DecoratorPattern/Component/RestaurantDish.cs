61f66cd683cf9a5f46ec864bd09233a1d65a0734
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-06-15 18:34:41 -0500

    component

---
+++ DesignPatterns/DecoratorPattern/Component/RestaurantDish.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecoratorPattern.Component
+{
+    public abstract class RestaurantDish
+    {
+        private string _name;
+        public string Name => _name;
+
+        private Dictionary<string, string> _ingredients;
+
+        public RestaurantDish(string name, Dictionary<string, string> ingredients)
+        {
+            _name = name;
+            _ingredients = ingredients;
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+
+            builder.AppendLine($"{Name}:");
+
+            foreach(var pair in _ingredients)
+            {
+                builder.AppendLine($" {pair.Key}: {pair.Value}");
+            }
+
+            return builder.ToString();
+        }
+    }
+}
