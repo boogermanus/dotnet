f9d9f25518e4aa5dab2c5c4fea4006ea69648055
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-06-18 15:25:33 -0500

    finish basic example

--- DesignPatterns/DecoratorPattern/Decorator/AvailableDecorator.cs
+++ DesignPatterns/DecoratorPattern/Decorator/AvailableDecorator.cs
@@ -1,14 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
 using DecoratorPattern.Component;
 
 namespace DecoratorPattern.Decarator
 {
-    public abstract class AvailableDecorator : RestaurantDish
+    public class AvailableDecorator : RestaurantDish
     {
         protected RestaurantDish _dish;
+        private List<string> _customers = new List<string>();
+        private int _numberAvailable = 0;
+
+        public AvailableDecorator(RestaurantDish dish, int available)
+            : this(dish)
+        {
+            _numberAvailable = available;
+        }
 
         public AvailableDecorator(RestaurantDish dish)
         {
             _dish = dish;
+        }
+
+        public void OrderItem(string name)
+        {
+            if(_numberAvailable > 0)
+            {
+                _customers.Add(name);
+                _numberAvailable--;
+            }
+            else
+            {
+                Console.WriteLine($"Not enough available for {name}'s order!");
+            }
+        }
+
+        public override string ToString() 
+        {
+            var builder = new StringBuilder();
+
+            builder.AppendLine(_dish.ToString());
+
+            foreach(var customer in _customers)
+            {
+                builder.AppendLine($"Ordered by {customer}");
+            }
+
+            return builder.ToString();
         }
     }
 }
--- DesignPatterns/DecoratorPattern/Program.cs
+++ DesignPatterns/DecoratorPattern/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using DecoratorPattern.ConcreteComponent;
+using DecoratorPattern.Decarator;
 
 namespace DecoratorPattern
 {
@@ -6,7 +9,46 @@ namespace DecoratorPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dish();
+        }
+
+        static void Dish()
+        {
+            FreshSalad ceasarSalad = new FreshSalad(
+                new Dictionary<string, string>
+                {
+                    {"Greens", "Crispy romaine lettuce"},
+                    {"Cheese", "Freshly-grated Parmasan cheese"},
+                    {"Dressing", "House-made Caesar dressing"}
+                }
+            );
+
+            Pasta fettuccineAlfredo = new Pasta(
+                new Dictionary<string, string>
+                {
+                    {"Pasta", "Fresh-made daily pasta"},
+                    {"Sauce", "Creamy garlic alfredo sauce"}
+                }
+            );
+
+            Console.WriteLine(ceasarSalad);
+            Console.WriteLine(fettuccineAlfredo);
+
+            AvailableDecorator caesarAvailable = new AvailableDecorator(ceasarSalad, 2);
+            AvailableDecorator alfredoAvailable = new AvailableDecorator(fettuccineAlfredo, 3);
+
+            caesarAvailable.OrderItem("Superman");
+            caesarAvailable.OrderItem("Batman");
+
+            alfredoAvailable.OrderItem("Wonder Women");
+            alfredoAvailable.OrderItem("Plastic Man");
+            alfredoAvailable.OrderItem("Flash");
+
+            // not enough
+            alfredoAvailable.OrderItem("Batman");
+
+            Console.WriteLine(caesarAvailable);
+            Console.WriteLine(alfredoAvailable);
         }
     }
 }
