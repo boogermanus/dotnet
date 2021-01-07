18d9911c8e71f422dc887e28c7c1790664f976ca
jordan.woodruff <jordan.woodruff@tylertech.com>
2021-01-07 11:52:05 -0600

    quick test of something

--- ConsolePlayground/Program.cs
+++ ConsolePlayground/Program.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Collections;
 using System.Security.Cryptography;
+using System.Xml.Linq;
 
 namespace ConsolePlayground
 {
@@ -10,31 +11,41 @@ namespace ConsolePlayground
     {
         static void Main(string[] args)
         {
-      
+
             // var date = DateTime.UtcNow;
             // Console.WriteLine(date);
             // date = TimeZoneInfo.ConvertTimeFromUtc(date, TimeZoneInfo.Local);
             // Console.WriteLine(date);
 
-            Console.WriteLine(GeneratePassword("GovPay"));
+            // Console.WriteLine(GeneratePassword("GovPay"));
+
+            TestXDocument();
         }
 
-        
-    public static string GeneratePassword(string userId)
-    {
-        string passwordKey = "1ncode123!" + userId;
-        var encoder = System.Text.Encoding.ASCII.GetEncoder();
 
-        byte[] data = new byte[passwordKey.Length];
+        public static string GeneratePassword(string userId)
+        {
+            string passwordKey = "1ncode123!" + userId;
+            var encoder = System.Text.Encoding.ASCII.GetEncoder();
 
-        encoder.GetBytes(passwordKey.ToCharArray(), 0, passwordKey.Length, data, 0, true);
+            byte[] data = new byte[passwordKey.Length];
 
-        MD5 md5 = new MD5CryptoServiceProvider();
+            encoder.GetBytes(passwordKey.ToCharArray(), 0, passwordKey.Length, data, 0, true);
 
-        byte[] result = md5.ComputeHash(data);
+            MD5 md5 = new MD5CryptoServiceProvider();
 
-        return BitConverter.ToString(result).Replace("-", "").ToLower();
-    }
+            byte[] result = md5.ComputeHash(data);
+
+            return BitConverter.ToString(result).Replace("-", "").ToLower();
+        }
+
+        public static void TestXDocument()
+        {
+            var doc = new XDocument(new XElement("transactionResult", new XAttribute("receiptNumber", "test")));
+
+            var result = doc.Descendants("transactionResult").SingleOrDefault();
+            Console.WriteLine(result);
+        }
     }
 
 }
