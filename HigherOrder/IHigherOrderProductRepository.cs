a62c4d0cc744d5874d7f4ac5c13eb45aab20b04c
jordan.woodruff <jordan.woodruff@tylertech.com>
2021-07-03 13:18:02 -0500

    add linq method

--- HigherOrder/IHigherOrderProductRepository.cs
+++ HigherOrder/IHigherOrderProductRepository.cs
@@ -7,5 +7,6 @@ namespace HigherOrder.Interfaces
     public interface IHigherOrderProductRepository
     {
         public IEnumerable<Product> Get(Func<Product, bool> filter = null);
+        public IEnumerable<Product> GetLinq(Func<Product, bool> filter = null);
     }
 }
