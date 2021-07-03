64a172a5af234de668553fe1b734dd61794b878d
jordan.woodruff <jordan.woodruff@tylertech.com>
2021-07-03 13:12:08 -0500

    more example code

---
+++ HigherOrder/HigherOrderProductRepository.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using HigherOrder.Interfaces;
+using HigherOrder.Models;
+
+namespace HigherOrder.Services
+{
+    public class HigherOrderProductRepository : IHigherOrderProductRepository
+    {
+        private readonly List<Product> _products = Product.GetProducts();
+        public IEnumerable<Product> Get(Func<Product, bool> filter = null)
+        {
+            if (filter == null)
+                return _products;
+
+            var filterList = new List<Product>();
+
+            _products.ForEach(p => {
+                if(filter(p))
+                    filterList.Add(p);
+            });
+
+            return filterList;
+        }
+
+        public IEnumerable<Product> GetLinq(Func<Product, bool> filter = null)
+        {
+            if(filter == null)
+                return _products;
+
+            return _products.Where(p => filter(p));
+        }
+    }
+}
