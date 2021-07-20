b30ac5e51638358385b6641435a53cec9fc16071
jordan.woodruff <jordan.woodruff@tylertech.com>
2021-07-20 15:28:23 -0500

    FakeItEasy example start

---
+++ FakeItEasy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FakeItEasy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
