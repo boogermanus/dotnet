ab7c17a05f41ec926cfb75bf05f82b8fabf63d30
jordan.woodruff <jordan.woodruff@tylertech.com>
2021-07-22 19:54:07 -0500

    BoardObject

---
+++ PrizeGame/BoardObject.cs
@@ -0,0 +1,18 @@
+﻿namespace PrizeGame
+{
+    public abstract class BoardObject
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        
+        // Indicates whether this object prevents an Player from moving in.
+        // @return True if this object prevents entry, false otherwise.
+        public abstract bool BlocksMovement();
+        
+        public void SetPosition(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
