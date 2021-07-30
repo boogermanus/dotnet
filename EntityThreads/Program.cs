57643ba383fbbde7e8dff2450e4324026499b113
jordan.woodruff <jordan.woodruff@tylertech.com>
2021-07-30 18:27:00 -0500

    Entity Threads initial commit

---
+++ EntityThreads/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
