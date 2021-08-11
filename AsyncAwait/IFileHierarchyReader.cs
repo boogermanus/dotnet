e9711c832a3eba6a2a1b0d428fa22fc7c276db05
jordan.woodruff <jordan.woodruff@tylertech.com>
2021-08-10 19:56:46 -0500

    initial commit

---
+++ AsyncAwait/IFileHierarchyReader.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    public interface IFileHierarchyReader
+    {
+        string ReadFileHierarchy(string file);
+        Task<string> ReadFileHierarchyAsync(string file);
+    }
+}
