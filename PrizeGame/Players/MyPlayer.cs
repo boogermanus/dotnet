41b9d46127fe3c13f3c52a6c110474a3f023e0b1
jordan.woodruff <jordan.woodruff@tylertech.com>
2022-02-21 21:11:38 -0600

    first solution

--- PrizeGame/Players/MyPlayer.cs
+++ PrizeGame/Players/MyPlayer.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PrizeGame.Players
 {
@@ -10,20 +12,65 @@ namespace PrizeGame.Players
 
         }
 
-        // Goal: Implement the "Move" method below.
-
-        // Try to win the game as often as possible.
-
-        // But, it is NOT possible to win every time. No code will be perfect.
-
-        // Ask me lots of questions! I am here to help.
-
-        // You can google anything you want.
-
         public override void Move(Board board)
         {
-            // TODO: remove the line below and write your own code
-            board.Move(this, board.GetAllowedMoves(X, Y).First());
+            Direction? direction = null;
+            var prizes = new List<PrizeTarget>();
+            
+            foreach (var prize in board.GetPrizes())
+            {
+                prizes.Add(new PrizeTarget(prize, Distance(prize)));
+            }
+            
+            // or
+            // var prizes = board.GetPrizes().Select(prize => new PrizeTarget(prize, Distance(prize))).ToList();
+
+            var targetList = prizes
+                .OrderBy(tl => tl.Distance)
+                .ThenByDescending(tl => tl.Points)
+                .ToList();
+
+            // use FirstOrDefault because what if all the other prizes have been consumed, and the list is empty?
+            var targetPrize = targetList
+                .FirstOrDefault();
+
+            // a bug -- maybe? rider flagged this as a possible null value because of the FirstOrDefault above
+            // if null was passed in, GetDirection should throw?
+            // direction = GetDirection(board, targetPrize.Prize);
+
+            // this is safe according to rider
+            if (targetPrize != null)
+                direction = GetDirection(board, targetPrize.Prize);
+            
+            if (direction.HasValue)
+            {
+                board.Move(this, direction.GetValueOrDefault());
+            }
+        }
+
+        private int Distance(BoardObject target)
+        {
+            return Math.Abs(target.X - X) + Math.Abs(target.Y - Y);
+        }
+    }
+
+    internal class PrizeTarget
+    {
+        private readonly Prize _prize;
+        
+        public PrizeTarget(Prize prize, int distance)
+        {
+            _prize = prize;
+            Distance = distance;
+        }
+
+        public int Points => _prize.Points;
+        public Prize Prize => _prize;
+        public int Distance { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Distance} - {Points}";
         }
     }
 }
