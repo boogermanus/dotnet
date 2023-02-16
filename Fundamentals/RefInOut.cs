bedb8f207660b20fbb5a3d1cb479d052b10d898f
jordan.woodruff <jordan.woodruff@tylertech.com>
2023-02-16 15:42:26 -0600

    fix ref in out

--- Fundamentals/RefInOut.cs
+++ Fundamentals/RefInOut.cs
@@ -3,10 +3,16 @@ using System.Runtime.CompilerServices;
 
 namespace Fundamentals
 {
-    public class RefInOut
+    public class RefInOut : IDemo
     {
         private static readonly ConditionalWeakTable<InternalClass, RefId> _ids = new ConditionalWeakTable<InternalClass, RefId>();
-        internal static void Test()
+
+        public void Run()
+        {
+            Test();
+        }
+
+        private void Test()
         {
             int i = 0;
             
@@ -39,77 +45,50 @@ namespace Fundamentals
             originalId = instance.GetRefId();
             ReferenceTypePassByIn(in instance);
             Console.WriteLine($"{nameof(ReferenceTypePassByIn)}: {instance.Name} {originalId == instance.GetRefId()}");
-
         }
 
 
-        internal static void ValueTypePassByValue(int i)
+        internal void ValueTypePassByValue(int i)
         {
             i++;
         }
         
-        internal static void ValueTypePassByRef(ref int i)
+        internal void ValueTypePassByRef(ref int i)
         {
             i++;
         }
         
-        internal static void ValueTypePassByOut(out int i)
+        internal void ValueTypePassByOut(out int i)
         {
             //var test = i == 0;  cannot read until set
             i = int.MaxValue;
         }
 
-        internal static void ValueTypePassByIn(in int i)
+        internal void ValueTypePassByIn(in int i)
         {
             // i is readonly
         }
 
-        private static void StringByValue(string t)
-        {
-            t = "New String";
-        }
-
-        private static void StringByRef(ref string t)
-        {
-            t = "New String";
-        }
-
-        private static void StringByOut(out string t)
-        {
-            // Cannot read until set
-            //if (string.IsNullOrEmpty(t))
-            t = "New String";
-        }
-
-        private static void StringByIn(in string t)
-        {
-            if (string.IsNullOrEmpty(t))
-            {
-                // Readonly
-                //t = "New String";
-            }
-        }
-
-        internal static void ReferenceTypePassByValue(InternalClass c)
+        internal void ReferenceTypePassByValue(InternalClass c)
         {
             c.Name = nameof(ReferenceTypePassByValue);
             
             c = new InternalClass(); // this instance is not used at all
         }
 
-        internal static void ReferenceTypePassByRef(ref InternalClass c)
+        internal void ReferenceTypePassByRef(ref InternalClass c)
         {
             c.Name = nameof(ReferenceTypePassByValue);
             c = new InternalClass { Name = c.Name + "Modified" };
         }
 
-        internal static void ReferenceTypePassByOut(out InternalClass c)
+        internal void ReferenceTypePassByOut(out InternalClass c)
         {
             //c.Name = nameof(ReferenceTypePassByOut);  cannot read until set
             c = new InternalClass { Name = nameof(ReferenceTypePassByOut) };
         }
 
-        internal static void ReferenceTypePassByIn(in InternalClass c)
+        internal void ReferenceTypePassByIn(in InternalClass c)
         {
             // c reference cannot be changed
             c.Name = nameof(ReferenceTypePassByIn);
@@ -125,8 +104,8 @@ namespace Fundamentals
             }
 
         }
-        
-        internal class RefId
+
+        private class RefId
         {
             public Guid Id { get; } = Guid.NewGuid();
         }
