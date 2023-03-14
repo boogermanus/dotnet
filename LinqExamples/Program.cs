b0b92098bb8f21a9f6361e94a3651b9f34200906
jordan.woodruff <jordan.woodruff@tylertech.com>
2023-03-14 17:49:06 -0500

    code cleanup

--- LinqExamples/Program.cs
+++ LinqExamples/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using LinqExamples.Demos;
 using LinqExamples.Interfaces;
-using LinqExamples.Models;
 
 namespace LinqExamples
 {
