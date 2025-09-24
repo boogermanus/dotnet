d2d2e9a7d5102645f2c2012ac1dcffa66ca9aac9
Jordan Woodruff <boogermanus@gmail.com>
2025-09-24 15:17:27 -0500

    using moq

--- MoqIt/MoqItTests/MoqServiceTests.cs
+++ MoqIt/MoqItTests/MoqServiceTests.cs
@@ -36,4 +36,34 @@ public class MoqServiceTests : IDisposable
         _mock.Setup(fs => fs.GetCount()).Returns(1);
         Assert.Equal(1, _mockService.GetCount());
     }
+
+    [Fact]
+    public void IncrementCountMockOverrides()
+    {
+        _mock.Setup(fs => fs.GetCount()).Returns(1);
+        Assert.Equal(1, _mockService.GetCount());
+        _mock.Setup(fs => fs.GetCount()).Returns(2);
+        Assert.NotEqual(1, _mockService.GetCount());
+    }
+
+    [Fact]
+    public void GetCountWithOptionAnyReturnsZero()
+    {
+        _mock.Setup(fs => fs.GetCount(It.IsAny<string>())).Returns(0);
+        Assert.Equal(0, _mockService.GetCount("test"));
+    }
+
+    [Fact]
+    public void GetCountWithZeroReturnsOne()
+    {
+        _mock.Setup(fs => fs.GetCount("zero")).Returns(1);
+        Assert.Equal(1, _mockService.GetCount("zero"));
+    }
+
+    [Fact]
+    public void GetCountMockThrows()
+    {
+        _mock.Setup(fs => fs.GetCount()).Throws<Exception>();
+        Assert.Throws<Exception>(() => _mockService.GetCount());
+    }
 }
